bb7e6b2f38019be3b8d6ef9e9db1be44c0e678c3
{Biswamber} <{beherabiswamber30@gmail.com}>
2026-07-14 17:18:40 +0530

    Responsive UI improvements

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p)or=x19v+pxza6u_d#ufp--w_qxftrf+z=n+odk$3_ehx2)yb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "https://noteverse-jrkt.onrender.com/",
