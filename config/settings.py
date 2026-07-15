474250f01538cd439d751e577dedd71d8c95c1ff
{Biswamber} <{beherabiswamber30@gmail.com}>
2026-07-15 12:04:26 +0530

    Add Cloudinary media storage

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'accounts',
     'notes',
+    "cloudinary_storage",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
@@ -135,4 +137,14 @@ LOGIN_URL = "login"
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+
+import os
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
